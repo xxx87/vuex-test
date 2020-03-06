41e30c43b44bf80d3a32f03a2260c7379d348133
i.isaev <i.isaev@pronet.ua>
2020-03-06 09:11:53 +0200

    CR

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="post" v-for="post in allPosts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {mapGetters} from 'vuex'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  computed: mapGetters(['allPosts']),
+  // computed: {
+  //   allPosts() {
+  //     return this.$store.getters.allPosts
+  //   }
+  // },
+  async mounted() {
+    let res = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?_limit=3"
+    );
+    let posts = await res.json();
+    this.posts = posts;
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
+}
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 </style>
---
+++ src/store/modules/post.js
@@ -0,0 +1,12 @@
+export default {
+  actions: {},
+  mutations: {},
+  state: {
+    posts: []
+  },
+  getters: {
+    allPosts(state) {
+      return state.posts
+    }
+  }
+};
